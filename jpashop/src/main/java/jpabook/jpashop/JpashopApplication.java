f11aa7482b9bcd9b6c383bfe2a286a300764579b
yhw0104 <yhw0104@naver.com>
2023-05-09 21:19:19 +0900

    1st commit

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
