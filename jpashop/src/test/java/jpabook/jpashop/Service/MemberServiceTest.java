e10bd83aa5fb1424cc27b746968e9776a6a3fd35
yhw0104 <yhw0104@naver.com>
2023-05-20 00:02:13 +0900

    6th commit

---
+++ jpashop/src/test/java/jpabook/jpashop/Service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);    //예외가 발생해야 한다.
+        } catch (IllegalStateException e){
+            return;
+        }
+        //then
+        fail("예외가 발생해야 한다.");   // memberService.join(member2)에서 예외가 나와야 하기 때문에 fail의 문자열이 나오면 안된다.
+
+
+    }
+}
