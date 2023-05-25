d2bddd79b39209465aed7ea538f4ba95003dfd2c
yhw0104 <yhw0104@naver.com>
2023-05-25 16:29:52 +0900

    8th commit

--- jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    // commit 주석
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!!");
